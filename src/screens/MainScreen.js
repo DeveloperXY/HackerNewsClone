32529ac9f9d37efb97ba19b2bde06bfe383a93fc
Mohammed Aouf Zouag <m.a.zouag@gmail.com>
2019-10-23 16:27:23 +0100

    Create a main screen & setup navigation

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const MainScreen = (props) => {
+    return (
+        <Text>Hello world !</Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default MainScreen;
