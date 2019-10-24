28d9cf498056fe06bdf548dd5d691d6f08d80dc0
Mohammed Aouf Zouag <m.a.zouag@gmail.com>
2019-10-24 11:03:41 +0100

    Show a loading progress indicator

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,24 +1,39 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList, ProgressBarAndroid, StyleSheet} from 'react-native';
+import {FlatList, ProgressBarAndroid, StyleSheet, View} from 'react-native';
 import Story from "../components/Story";
 import {getStoryById, getStoryIds} from "../api/storiesApi";
 
 const MainScreen = () => {
     const [stories, setStories] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         getStoryIds().then(ids => ids.map(getStoryById))
             .then(data => Promise.all(data))
-            .then(setStories);
+            .then(setStories)
+            .then(() => {
+                setIsLoading(false);
+            })
+            .catch(error => {
+                setIsLoading(false);
+            });
     }, []);
 
-    return <FlatList
+    return isLoading ? <View style={styles.progressBarWrapper}>
+        <ProgressBarAndroid color="#FF6600"/>
+    </View> : <FlatList
         data={stories}
         keyExtractor={story => story.id.toString()}
         renderItem={({item: story, index}) => <Story story={story} index={index + 1}/>}/>;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    progressBarWrapper: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    }
+});
 
 MainScreen.navigationOptions = {
     title: 'Hacker News',
