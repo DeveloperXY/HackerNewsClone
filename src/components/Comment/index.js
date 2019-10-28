e14ee6785b396998a94a1217d633445335797f01
Mohammed Aouf Zouag <m.a.zouag@gmail.com>
2019-10-28 11:42:41 +0100

    Add a 'view replies' button to any threaded comment

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import {timestamp2TimeAgo} from "../../utils/helpers";
 import HTML from "react-native-render-html";
+import {colorDark} from "../../utils/colors";
+import Ripple from "react-native-material-ripple";
 
 const Comment = ({comment}) => {
-    return <View style={styles.wrapper}>
+
+    function isThreaded() {
+        return comment.kids && comment.kids.length !== 0;
+    }
+
+    return <View style={styles.hWrapper}>
         <Image style={styles.arrowStyle} source={require('../../../assets/grayarrow2x.gif')}/>
         <View style={styles.body}>
             <Text style={styles.subheader}>@{comment.by}, {`${timestamp2TimeAgo(comment.time)}`}</Text>
             <HTML key={comment.id} html={comment.text}/>
+            {
+                isThreaded() && <Ripple rippleColor={colorDark} style={{alignSelf: 'flex-start'}}>
+                    <Text style={styles.subAction}>View replies</Text>
+                </Ripple>
+            }
         </View>
     </View>;
 };
@@ -23,12 +35,17 @@ const styles = StyleSheet.create({
         marginTop: 4,
         marginRight: 4
     },
-    wrapper: {
+    hWrapper: {
         flexDirection: 'row',
-        paddingVertical: 8
+        marginBottom: 16,
     },
     body: {
         flex: 1
+    },
+    subAction: {
+        color: colorDark,
+        fontWeight: 'bold',
+        margin: 8
     }
 });
 
